a1d0454402fce7f522aeadafd9bf97afcb76145c
runphilrun <myotheroctopus@gmail.com>
2016-07-18 01:49:35 -0700

    perform operations outside of functions for now

--- NozzleDesign.py
+++ NozzleDesign.py
@@ -5,19 +5,32 @@ Philip Linden
 """
 
 import configparser
+import math
 
 # set global constants
 g0 = 9.81  # Standard gravity [m/s^2]
-R = .008314  # Universal gas constant [J/K*kmol]
+R0 = .008314  # Universal gas constant [J/K*kmol]
 # load values from config.ini
 parser = configparser.ConfigParser()
 parser.read('config.ini')
 # propellant properties
+propName = parser.get('Propellant', 'name')  # prop name
 k = parser.getfloat('Propellant', 'k')  # specific heat ratio
 mMol = parser.getfloat('Propellant', 'mMol')  # molecular weight
 # nozzle properties
+nozMatl = parser.get('Nozzle', 'matl')  # nozzle material name
 thk = parser.getfloat('Nozzle', 'thk')  # nozzle thickness
 rhoNoz = parser.getfloat('Nozzle', 'rhoNoz')  # nozzle density
 # chamber properties
 Tc = parser.getfloat('Chamber', 'Tc')  # chamber temp
 Pc = parser.getfloat('Chamber', 'Pc')  # chamber pressure
+
+# print summary
+print('Propellant:', propName)
+print('Chamber:', Tc, 'K,', Pc, 'bar')
+print('Nozzle:', nozMatl, '@', thk, 'm thickness')
+
+# get inputs
+mdot = float(input('Mass flow rate [kg/s]: '))  # mass flow rate [kg/s]
+maxD = float(input('Max nozzle exit diameter [cm]: '))
+maxAe = math.pi*pow(2, .5*maxD/100)  # max exit area [m]
