06d1c1c5dbdaadd0a048e3ec8a8c5e7b527dbb45
Nate-Wilkins <nwilkins2012@gmail.com>
2016-07-26 00:40:09 -0400

    (tech) use sphinx doctest instead of pytest

--- setup.py
+++ setup.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
-from setuptools import setup, find_packages
-# To use a consistent encoding
-from codecs import open
 from os import path
+from codecs import open
+from setuptools import setup, find_packages
 
 here = path.abspath(path.dirname(__file__))
 
@@ -11,17 +10,17 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='thrusted',
-    version='0.0.1',
-    description='Thruster nozzle design tool written.',
-    long_description=long_description,
-    url='https://github.com/runphilrun/ThrusterDesign',
-    author='Phil-Linden',
-    author_email='pjl7651@rit.edu',
-    license='Apache-2.0',
+    name = 'thrusted',
+    version = '0.0.1',
+    description = 'Thruster nozzle design tool written.',
+    long_description = long_description,
+    url = 'https://github.com/runphilrun/ThrusterDesign',
+    author = 'Phil-Linden',
+    author_email = 'pjl7651@rit.edu',
+    license = 'Apache-2.0',
 
-    # https://pypi.python.org/pypi?%3Aaction=list_classifiers
-    classifiers=[
+    # https://pypi.python.org/pypi?%3Aaction = list_classifiers
+    classifiers = [
         'Development Status :: 1 - Planning',
         'Intended Audience :: Science/Research',
         'Topic :: Scientific/Engineering',
@@ -31,16 +30,16 @@ setup(
         'Programming Language :: Python :: 3 :: Only'
     ],
 
-    keywords='',
+    keywords = '',
 
-    package_dir={'': 'src'},
-    packages=find_packages('src'),
-    setup_requires=['pytest-runner', 'check-manifest'],
-    tests_require=['pytest', 'check-manifest', 'coverage'],
+    package_dir = {'': 'src'},
+    packages = find_packages('src'),
+    setup_requires = ['check-manifest', 'sphinx', 'sphinx_rtd_theme'],
+    tests_require = ['sphinx', 'check-manifest', 'coverage'],
 
-    entry_points={
+    entry_points = {
         'console_scripts': [
-            'thrusted=thrusted.cli:main',
+            'thrusted = thrusted.cli:main'
         ]
     }
 )
