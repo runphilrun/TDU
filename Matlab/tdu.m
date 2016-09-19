eba4a354932b8b467533c13c3c09f4d3799ce50f
runphilrun <myotheroctopus@gmail.com>
2016-09-19 02:47:34 -0400

    format output
    format outputs into a table

--- Matlab/tdu.m
+++ Matlab/tdu.m
@@ -4,20 +4,28 @@ function varargout = tdu % main function
     %% universal constants
     universal_gas_constant= 8.3144598; % [J/(mol*K)]
     standard_gravity = 9.81; % [m/s^2]
+    unitless='[-]';
+    nounits='';
     
 %%  === EDIT THESE ===
     % gas properties of propellant
+    propellant_name = 'Air';
     specific_heat_ratio = 1.4; % 1.4 for air
-    molar_mass = .0289645; % [kg/mol]
+    molar_mass = .0289645; 
+    molar_mass_units = '[kg/mol]';
     
     % chamber conditions
-    chamber_temperature = 273; % [K]
-    chamber_pressure = 1; % [atm]
+    chamber_temperature = 273;
+    temperature_units = '[K]';
+    chamber_pressure = 1;
+    pressure_units = '[atm]';
     
     % nozzle geometry
-    exit_radius = .025; %[m] radius at nozzle exit
-    throat_radius = .010; %[m] radius at nozzle throat
-    conical_half_angle = 15; %[degrees] half angle of conical nozzle, 15 degrees is optimal
+    exit_radius = .025; % radius at nozzle exit
+    throat_radius = .010; % radius at nozzle throat
+    length_units = '[m]';
+    conical_half_angle = 15; % half angle of conical nozzle, 15 degrees is optimal
+    angle_units = '[deg]';
 %   ===------------===   
 
 %% Math (the fun part)
@@ -34,17 +42,19 @@ function varargout = tdu % main function
 %              \-
 %   ********************************
 
-    specific_gas_constant = universal_gas_constant/molar_mass; %[J/kgK]
-    exit_area = pi*exit_radius^2; % [m^2]
-    throat_area = pi*throat_radius^2; % [m^2]
+    specific_gas_constant = universal_gas_constant/molar_mass; 
+    specific_gas_constant_units = '[J/kg K]';
+    exit_area = pi*exit_radius^2; 
+    throat_area = pi*throat_radius^2; 
+    area_units = '[m^2]';
     length = (exit_radius-throat_radius)/tan(deg2rad(conical_half_angle)); % [m]
     
     %% Throat conditions
     % We are designing a nozzle that "chokes" the fluid flow at the throat,
     % so we assume that at the throat, Mach number is isentropically
     % brought to the boundary between supersonic and subsonic flow (Mach=1)
-    throat_temperature = chamber_temperature*(2/(specific_heat_ratio+1)) %[K]
-    throat_pressure = chamber_pressure*(2/(specific_heat_ratio+1))^(specific_heat_ratio/(specific_heat_ratio-1)); %[atm]
+    throat_temperature = chamber_temperature*(2/(specific_heat_ratio+1)); 
+    throat_pressure = chamber_pressure*(2/(specific_heat_ratio+1))^(specific_heat_ratio/(specific_heat_ratio-1)); 
     
     % use nozzle areas to find Mach number at the exit via the Newton-Raphson method
     exit_mach = solve_mach(exit_area,throat_area,specific_heat_ratio);
@@ -54,7 +64,8 @@ function varargout = tdu % main function
     throat_density = throat_pressure/(specific_gas_constant*throat_temperature); %[kg/m^3]
 %   and at the throat Mach = 1 so flow rate (speed of flow) is the fluid's speed of sound at the throat
     throat_flowrate = sqrt(specific_heat_ratio*specific_gas_constant*throat_temperature);
-    mass_flowrate = throat_density*throat_flowrate*throat_area; %[kg/s]
+    mass_flowrate = throat_density*throat_flowrate*throat_area; 
+    mass_flowrate_units = '[kg/s]';
 
     %% Exit conditions
 %   it's much easier to define the flow characteristics at the exit in
@@ -68,14 +79,43 @@ function varargout = tdu % main function
     temperature_ratio = 1+((specific_heat_ratio-1)/2)*exit_mach^2;
     pressure_ratio = temperature_ratio^((specific_heat_ratio-1)/specific_heat_ratio);
     
-    exit_temperature = chamber_temperature/temperature_ratio; % [K]
-    exit_pressure = chamber_pressure/pressure_ratio; % [atm]
+    exit_temperature = chamber_temperature/temperature_ratio; 
+    exit_pressure = chamber_pressure/pressure_ratio; 
     
-    exit_velocity = sqrt((2*specific_heat_ratio*specific_heat_ratio*chamber_temperature)/(specific_heat_ratio-1)*(1-1/(1+(specific_heat_ratio-1)/2*exit_mach^2))); % [m/s]
+    exit_velocity = sqrt((2*specific_heat_ratio*specific_heat_ratio*chamber_temperature)/(specific_heat_ratio-1)*(1-1/(1+(specific_heat_ratio-1)/2*exit_mach^2)));
+    velocity_units = '[m/s]';
     
     %% Thrust and Specific Impulse
-    thrust = mass_flowrate*exit_velocity + exit_pressure*exit_area; %[N]
+    thrust = mass_flowrate*exit_velocity + exit_pressure*exit_area;
+    force_units = '[N]';
     specific_impulse = thrust/(mass_flowrate*standard_gravity);
+    isp_units='[s]';
+    
+    %% format & display outputs
+    result =  {'Propellant','',propellant_name;
+               'Specific heat ratio', specific_heat_ratio, unitless;
+               'Molar mass', molar_mass, molar_mass_units;
+               'Specific gas constant',specific_gas_constant,specific_gas_constant_units;
+               'Chamber temperature', chamber_temperature, temperature_units;
+               'Chamber pressure', chamber_pressure, pressure_units;
+               'Exit radius', exit_radius, length_units;
+               'Throat radius', throat_radius, length_units;
+               'Half-angle',conical_half_angle, angle_units;
+               'Length',length,length_units;
+               'Exit area',exit_area,area_units;
+               'Throat area',throat_area,area_units;
+               'Throat temperature',throat_temperature,temperature_units;
+               'Throat pressure',throat_pressure,pressure_units;
+               'Mass flow rate',mass_flowrate,mass_flowrate_units;
+               'Exit Mach',exit_mach,unitless;
+               'Exit temperature',exit_temperature,temperature_units;
+               'Exit pressure',exit_pressure,pressure_units;
+               'Exhaust velocity',exit_velocity,velocity_units;
+               'Thrust',thrust,force_units;
+               'Specific impulse',specific_impulse,isp_units;
+               }; 
+
+    display(result);
 end
 
 function Mach = solve_mach(A,At,k)
@@ -99,11 +139,9 @@ function Mach = solve_mach(A,At,k)
     Mach = 1/sqrt(X);
 end
 
-function displaystring = display(parameter_array)
-    % expects
-    %   parameter_array = [name, value, units]
-    % of types
-    %  [3-column array] = [string, double, string]
-    
-    
+function display(result)
+    [n,~]=size(result);
+    for i = 1:n 
+        fprintf('\n%24s\t%12d\t%s',result{i,:});
+    end
 end
